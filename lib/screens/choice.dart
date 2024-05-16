fc48ddf2ea118dbdc0686dc139bf4e87aa4d516e
Farah Fairuz <farahfairuz014@gmail.com>
2024-05-16 08:15:37 +0700

    refactor : initial commit

---
+++ lib/screens/choice.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'register.dart';
+
+class ChoiceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.6), // Ganti dengan sesuai kebutuhan
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0), // Bentuk bulat
+                ),
+                backgroundColor: Colors.blue[900], // Warna biru tua
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                child: Text(
+                  'Masuk',
+                  style: TextStyle(color: Colors.white), // Warna teks putih
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0), // Bentuk bulat
+                ),
+                backgroundColor: Color(0xFFE9F6FE),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                child: Text('Daftar'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
